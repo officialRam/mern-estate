7e7844fe1b8713af1dbff60ae4d17e102b601b95
officialRam <109956264+officialRam@users.noreply.github.com>
2024-04-27 04:35:02 +0530

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js'
 dotenv.config();
 const app=express();
 
@@ -15,3 +16,4 @@ mongoose
 app.listen(3000, ()=>{
     console.log("Server is running on 3000!")
 })
+ app.use('/api/user', userRouter)
