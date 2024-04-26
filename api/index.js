976e8fc7a6f9faf6c7ba13cfdf53d84bed6f1890
officialRam <109956264+officialRam@users.noreply.github.com>
2024-04-27 00:54:09 +0530

    MongoDB is connected

---
+++ api/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+const app=express();
+
+mongoose
+.connect(process.env.MONGO)
+.then(()=>{
+    console.log("Database is connected")
+}).catch(()=>{
+    console.log("Database is not connected") 
+})
+
+app.listen(3000, ()=>{
+    console.log("Server is running on 3000!!")
+})
