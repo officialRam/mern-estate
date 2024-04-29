05c335906b565c59d88bf26ab96de335f5c29fc7
officialRam <109956264+officialRam@users.noreply.github.com>
2024-04-30 03:01:14 +0530

    add redux-persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import userReducer from './userSlice'
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const rootReducer=combineReducers({user:userReducer});
+const persistConfig = {
+    key: 'root',
+    storage,
+    // Optionally, specify any reducer keys that you do not want to persist here
+    version: 1,
+  };
+  const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {user:userReducer},
+  reducer: persistedReducer,
+//   here middleware is used to tell that the data is not serializable
   middleware:(getDefaltMiddleware)=>
     getDefaltMiddleware({
         serializableCheck:false,
     })
   
 })
+
+export const persistor = persistStore(store);
